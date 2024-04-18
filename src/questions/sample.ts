6506d61ce64401be24ccc2e915a7974956d9c5a7
Dawid Banachowski <bajkull@gmail.com>
2024-04-18 19:55:50 +0200

    feat: mock data

---
+++ src/questions/sample.ts
@@ -0,0 +1,166 @@
+import { Category } from "../components/board/types";
+
+export const sampleCategory: Category = {
+  id: "sampleCategory",
+  name: "Sample category",
+  gradient: "blue",
+  questions: [
+    {
+      question: "Test",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test3",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test4",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test5",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+  ],
+};
+
+export const sampleCategory2: Category = {
+  id: "sampleCategory",
+  name: "Sample category",
+  gradient: "indigo",
+  questions: [
+    {
+      question: "Test",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test3",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test4",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test5",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+  ],
+};
+
+export const sampleCategory3: Category = {
+  id: "sampleCategory",
+  name: "Sample category",
+  gradient: "lime",
+  questions: [
+    {
+      question: "Test",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test3",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test4",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test5",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+  ],
+};
+
+export const sampleCategory4: Category = {
+  id: "sampleCategory",
+  name: "Sample category",
+  gradient: "pink",
+  questions: [
+    {
+      question: "Test",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test3",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test4",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test5",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+  ],
+};
+
+export const sampleCategory5: Category = {
+  id: "sampleCategory",
+  name: "Sample category",
+  gradient: "purple",
+  questions: [
+    {
+      question: "Test",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test3",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test4",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+    {
+      question: "Test5",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: "2",
+    },
+  ],
+};
