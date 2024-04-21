a4c657a7dbd4507580d513576eb7c9df4a76bb92
Dawid Banachowski <bajkull@gmail.com>
2024-04-21 14:31:13 +0200

    feat: display question modal

--- src/components/board/components/question/Question.tsx
+++ src/components/board/components/question/Question.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import Button from "../../../button/Button";
 import { Question as QuestionType } from "../../types";
+import { useState } from "react";
+import RippleModal from "../../../modal/RippleModal";
 
 interface QuestionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   data: QuestionType;
@@ -9,14 +11,42 @@ interface QuestionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Question = ({ color, data, fallbackPoints, ...props }: QuestionProps) => {
+  const [isQuestionOpen, setIsQuestionOpen] = useState(false);
   const { points, answers, correctAnswer, question } = data;
 
-  const cls = classNames("py-3 bg-gradient-to-br shadow-lg", color);
+  const clsBtn = classNames("py-3 bg-gradient-to-br shadow-lg", color);
+  const clsContainer = classNames(
+    "mx-auto max-w-screen-2xl px-5 py-10 w-full h-full flex flex-col",
+    {
+      "items-center justify-center": !answers?.length,
+    }
+  );
+  const openQuestion = () => setIsQuestionOpen(true);
+  const closeQuestion = () => setIsQuestionOpen(false);
+
   return (
-    <Button {...props} className={cls} noBackground>
-      ${points ?? fallbackPoints}
-      <p className="text-"></p>
-    </Button>
+    <>
+      <RippleModal color={color} close={closeQuestion} show={isQuestionOpen}>
+        <div className={clsContainer}>
+          <h1 className="text-white text-center text-4xl font-medium">
+            {question}
+          </h1>
+          {answers && (
+            <div className="grid grid-cols-2 gap-5 mt-auto">
+              {answers?.map((answer) => (
+                <Button key={answer} outline className="w-full py-4 text-lg">
+                  {answer}
+                </Button>
+              ))}
+            </div>
+          )}
+        </div>
+      </RippleModal>
+      <Button {...props} className={clsBtn} noBackground onClick={openQuestion}>
+        ${points ?? fallbackPoints}
+        <p className="text-"></p>
+      </Button>
+    </>
   );
 };
 
