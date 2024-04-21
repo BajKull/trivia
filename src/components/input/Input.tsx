cb10d0e61ff40f15d6e9d91a91f5dd9b9afa3c90
Dawid Banachowski <bajkull@gmail.com>
2024-04-21 16:40:58 +0200

    feat: input component

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  noBackground?: boolean;
+  light?: boolean;
+  outline?: boolean;
+}
+
+const Input = ({
+  className,
+  light,
+  noBackground,
+  outline,
+  ...props
+}: InputProps) => {
+  const cls = classNames(
+    className,
+    "rounded-lg py-2 px-3 font-semibold transition-colors",
+    {
+      "bg-indigo-700 hover:bg-indigo-800 placeholder-slate-100 text-white":
+        !noBackground && !outline && !light,
+      "border-4": outline,
+      "bg-white text-slate-800": light,
+    }
+  );
+  return <input className={cls} {...props} />;
+};
+
+export default Input;
