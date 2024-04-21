4d018023d8056e05c574e8af9a5c610bd33bc4c3
Dawid Banachowski <bajkull@gmail.com>
2024-04-21 14:30:18 +0200

    feat: button outline prop

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,20 +1,23 @@
 import classNames from "classnames";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  noBackground: boolean;
+  noBackground?: boolean;
+  outline?: boolean;
 }
 
 const Button = ({
   className,
   children,
   noBackground,
+  outline,
   ...props
 }: ButtonProps) => {
   const cls = classNames(
-    "text-white rounded py-1 px-3 font-semibold",
+    "text-white rounded-lg py-1 px-3 font-semibold",
     className,
     {
-      "bg-indigo-700": !noBackground,
+      "bg-indigo-700 hover:bg-indigo-800": !noBackground && !outline,
+      "border-4": outline,
     }
   );
 
